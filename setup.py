34647cf0f25f18ad47a416663b3dd8c05f3eb32e
Frederico Caroli <fredtcaroli@gmail.com>
2019-03-09 20:22:45 -0300

    Issue #1: Adding dependencies info

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='ezCV',
     version='0.0',
     packages=find_packages(),
     author='Frederico Caroli',
     description='Backend library for ezCV',
-    test_requires=['pytest']
+    python_requires='>=3',
+    install_requires=['numpy', 'pyyaml', 'opencv-python>=3']
 )
