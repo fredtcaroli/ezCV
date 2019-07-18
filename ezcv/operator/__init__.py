e759eaef3bf276fd31ac79ad7e014e9316f523c3
Frederico Caroli <fredtcaroli@gmail.com>
2019-07-18 19:07:49 -0300

    Fixing operator registry type hints

--- ezcv/operator/__init__.py
+++ ezcv/operator/__init__.py
@@ -7,11 +7,11 @@ from . import implementations
 _OPERATORS = list()
 
 
-def get_available_operators() -> List[Operator]:
+def get_available_operators() -> List[Type[Operator]]:
     return _OPERATORS.copy()
 
 
-def register_operator(cls: Type[Operator]):
+def register_operator(cls: Type[Operator]) -> Type[Operator]:
     if not issubclass(cls, Operator):
         raise ValueError("%s is not an Operator" % str(cls))
     _OPERATORS.append(cls)
